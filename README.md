35beaa6113998b768b5c20fc39e83984489efa67
boris <boris.isac@webtech87.com>
2025-05-21 09:17:43 +0100

    [INIT] exercise

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Exercic_4
 dict exervise
+# Description:
+
+Given a list of strings words, return a dictionary where the key is a word and the value is the number of times that word appears in the list.
---
+++ main.py
@@ -0,0 +1,9 @@
+frutes = ["apple", "banana", "cherry","apple", "banana", "apple", "orange", "banana", "apple"]
+animals = ["dog", "cat", "dog", "dog", "bird", "cat", "fish"]
+scool_obj = ["book", "pen", "notebook", "pen", "book", "eraser", "pen", "pencil"]
+obj = ["book", "pen", "notebook", "1", "book", "eraser", 3, "pencil"]
+
+
+
+class Solution:
+    def count_words(self):
